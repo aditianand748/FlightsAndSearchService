0f617a8cf035678ad54bce8bf4f4ca82a52a53d5
Aditi <aditianand748@gmail.com>
2025-10-10 13:14:43 +0530

    Added airplanes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
 const ApiRoutes = require('./routes/index');
-const CityRepository = require('./repository/city-repository');
+//const CityRepository = require('./repository/city-repository');
+
+const db = require('./models/index');
+//const sequelize = require('sequelize');
+const {Airplane} = require('./models/index');
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -11,19 +15,16 @@ const setupAndStartServer = async () => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
 app.use('/api', ApiRoutes);
-
-
 
     app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
-        
-        
-  
-});
-
+       if(process.env.SYN_DB) {
+        db.sequelize.sync({alter: true});
+       }
+      
+      
+    });
 }
 
 setupAndStartServer();
