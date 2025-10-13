1e223a555a125296a8d9f88c3dc4f3b1d2247e75
Aditi <aditianand748@gmail.com>
2025-10-14 00:11:00 +0530

    Added flight-create request middleware

---
+++ src/middlewares/flight-middlewares.js
@@ -0,0 +1,26 @@
+const validateCreateFlight = (req, res, next) => {
+    if(
+        !req.body.flightNumber ||
+        !req.body.airplaneId ||
+        !req.body.price ||
+        !req.body.departureAirportId ||
+        !req.body.arrivalAirportId ||
+        !req.body.arrivalTime ||
+        !req.body.departureTime
+    ) {
+
+        return res.status(400).json({
+            data: {},
+            success: false,
+            message: 'Invalid request body for create a flight',
+            err: 'Missing mandatory properties to create a flight'
+        });
+
+    }
+
+    next();
+}
+
+module.exports = {
+    validateCreateFlight
+}
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const { FlightMiddlewares } = require('../../middlewares/index');
+
 const CityController = require('../../controllers/city-controller');
 const FlightController = require('../../controllers/flight-controller.js');
 const AirportController = require('../../controllers/airport-controller');
@@ -10,8 +12,12 @@ router.delete('/city/:id', CityController.destroy);
 router.get('/city/:id', CityController.get);
 router.get('/city', CityController.getAll);
 router.patch('/city/:id', CityController.update);
+router.post(
+    '/flights', 
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);
 
-router.post('/flights', FlightController.create);
 router.get('/flights', FlightController.getAll);
 
 router.post('/airports', AirportController.create);
